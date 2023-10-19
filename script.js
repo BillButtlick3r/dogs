384b5b04538b8270192cc94c7e7ed677fddbb3b0
BillButtlick3r <146017379+BillButtlick3r@users.noreply.github.com>
2023-10-19 11:08:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("patrick.jpeg")) {
+      image.src = "spongebob.jpeg";
+    } else {
+      image.src = "patrick.jpeg";
+    }
+  }
